2b2866722e5135e1e3874a66344999037ccd477e
tstrickler4 <strickler.tanner@gmail.com>
2023-02-23 10:52:19 -0600

    Update streamlit_app.py
    Added input box for search term.

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,9 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
 # streamlit.text(fruityvice_response.json())
 
 # Take the json response and normalize it. 
