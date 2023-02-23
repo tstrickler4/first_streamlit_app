7df3e3fe1be01172220f746a4858833f12c2e188
tstrickler4 <strickler.tanner@gmail.com>
2023-02-23 15:31:25 -0600

    Update streamlit_app.py
    Testing insert from streamlit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,5 +40,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The list contains:")
 streamlit.dataframe(my_data_rows)
 
-fruit_choice2 = streamlit.text_input('What fruit would you like information about?','Jackfruit')
-streamlit.write('The user entered ', fruit_choice2)
+add_my_fruit = streamlit.text_input('What fruit would you like information about?','Jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
