1ff7f1b76e3c89f0563a3836ef60f7dfc73ee6d7
tstrickler4 <strickler.tanner@gmail.com>
2023-02-23 15:41:14 -0600

    Update streamlit_app.py
    Fixed typo.

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,9 @@ try:
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information.')
   else:
-  fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
-  fruityvice_noramlized = pandas.json_normalize(fruityvice_response.json())
-  streamlit.dataframe(fruityvice_normalized)
+    fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
+    fruityvice_noramlized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
 # streamlit.text(fruityvice_response.json())
