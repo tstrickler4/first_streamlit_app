4a7ed34e84708b44696a6641c4ac4496d9ca657f
tstrickler4 <strickler.tanner@gmail.com>
2023-02-23 15:40:41 -0600

    Update streamlit_app.py
    Added error handling to first text box.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,16 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get information.')
+  else:
+  fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
+  fruityvice_noramlized = pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 # streamlit.text(fruityvice_response.json())
 
 # Take the json response and normalize it. 
